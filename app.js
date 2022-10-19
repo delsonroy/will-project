85a6ec7ee56c2971c3e8976d481615249d8c0d1d
prabhjot <prabhjotsingh2598@gmail.com>
2022-10-19 00:42:04 -0400

    add the new aws mongo database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/login');
@@ -10,6 +11,7 @@ var usersRouter = require('./routes/user');
 var admin = require('./routes/admin')
 var index = require('./routes/index')
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,9 +23,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const url="mongodb://localhost:27017/Willproject"
+
+
+// const url="mongodb://18.209.70.80:27017/willprojectdb"
+const url =process.env.url1
+
 // const url="mongodb+srv://prabhjot:123@cluster0.6tgh1i7.mongodb.net/?retryWrites=true&w=majority"
+// const url  = "mongodb+srv://prabhjot:123@cluster0.jbxelza.mongodb.net/?retryWrites=true&w=majority"
 // const url="mongodb+srv://prabhjot:123@willproject.9iyxjmj.mongodb.net/?retryWrites=true&w=majority"
+// const url= "mongodb://admin:CH4lfxWdceONAOea@SG-wilproject-54518.servers.mongodirector.com:27017/admin?ssl=true"
+// const url ="mongodb+srv://prabhjot:123@cluster0.qxjkwao.mongodb.net/?retryWrites=true&w=majority"
+
+  
 mongoose.connect(url,{useNewUrlParser:true,   
 })
 
